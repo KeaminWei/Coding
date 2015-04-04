c90a023ae7ebd7e806026ee38eecee85006ebc9b
KeaminWei <keaminwei@gmail.com>
2015-04-04 10:05:28 +0800

    Add a py

---
+++ mon.py
@@ -0,0 +1,23 @@
+mon=[
+	"january",
+	"february",
+	"march",
+	"april",
+	"may",
+	"june",
+	"july",
+	"august",
+	"sep",
+	"oct",
+	"nov",
+	"dec",
+]
+endings=['st','nd','rd']+17*['th']+['st','nd','rd']+7*['th']+['st']
+year=raw_input('year:')
+month=raw_input('month:')
+day=raw_input('day:')
+month_num=int(month)
+day_num=int(day)
+month_name=mon[month_num-1]
+ordinal=day+endings[day_num-1]
+print month_name+' '+ordinal+' '+year
